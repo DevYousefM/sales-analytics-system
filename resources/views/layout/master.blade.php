0e613afefd3bf33d0eac47b962cf6f3432306f2b
devyousefm <devyousefm@gmail.com>
2025-05-11 03:06:43 +0300

    Wrap Nav Items

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -22,7 +22,7 @@
 <body
     class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-        <nav class="flex items-center justify-center gap-4">
+        <nav class="flex flex-wrap items-center justify-center gap-4">
             <a href="{{ route('dashboard') }}"
                 class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
                 Dashboard
@@ -39,8 +39,13 @@
                 class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
                 AI Recommend
             </a>
+            <a href="{{ route('suggestions') }}"
+                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                Suggestions
+            </a>
         </nav>
     </header>
+
     <div
         class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
         <main class="flex w-3/4 lg:w-3/4 md:w-3/4 ">
