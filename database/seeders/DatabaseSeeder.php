ca23a192b26261d69fa5ac0c3ea7a7bc80f9f688
devyousefm <devyousefm@gmail.com>
2025-05-11 23:38:10 +0300

    fix running seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ProductSeeder::class, OrderSeeder::class);
+        $this->call([ProductSeeder::class, OrderSeeder::class]);
     }
 }
